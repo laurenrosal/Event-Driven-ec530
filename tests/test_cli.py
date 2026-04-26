3206c8ce3b02a9809e97d666d05b1734246452db
laurenrosal <103148328+laurenrosal@users.noreply.github.com>
2026-04-26 17:39:45 -0400

    edit my test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,7 @@
-from Services.cli_service.cli import upload, search
+from Services.cli_service.cli import upload_image, search_images
 
 def test_upload_runs():
-    upload("images/test.jpg")
+    upload_image("images/test.jpg")
 
 def test_search_runs():
-    search("a cat wearing a hat")
+    search_images("a cat wearing a hat")
